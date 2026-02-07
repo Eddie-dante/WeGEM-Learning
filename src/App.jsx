22ee851e360ef7f80ab95de4f2586137e868a824
Eddie-dante <eddiegucci05@gmail.com>
2026-02-07 03:23:52 -0800

    Create src/App.jsx for WeGEM Learning main app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeGEMLearningApp = () => {
+    return (
+        <div>
+            <h1>Welcome to WeGEM Learning</h1>
+            <p>Your journey to knowledge starts here!</p>
+        </div>
+    );
+};
+
+export default WeGEMLearningApp;
