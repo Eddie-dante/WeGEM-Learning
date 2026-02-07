7c6ed6a81c6d7f56acd6e03cd36143a78f27a96f
Eddie-dante <eddiegucci05@gmail.com>
2026-02-07 03:24:26 -0800

    Create src/index.js React entry point for WeGEM Learning app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Entry point for the WeGEM Learning app
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render the application
+ReactDOM.render(<App />, document.getElementById('root'));
